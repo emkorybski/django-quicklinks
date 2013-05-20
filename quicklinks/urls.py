ab5dad54d9e12c991b743a8784a99961f5254ca6
michal <emkorybski@gmail.com>
2013-05-20 16:24:25 +0100

    added new url

--- quicklinks/urls.py
+++ quicklinks/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'quicklinks.views.links', name='links'),
+    url(r'^quicklinks/$', 'quicklinks.views.links', name='links'),
 )
